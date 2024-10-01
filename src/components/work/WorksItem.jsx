19624790638a504526296e7e3a0b1ec1bbb18016
soundar2356 <163382563+soundar2356@users.noreply.github.com>
2024-10-01 15:04:18 +0100

    changing the minor mistakes in words section

--- src/components/work/WorksItem.jsx
+++ src/components/work/WorksItem.jsx
@@ -6,7 +6,7 @@ const WorksItem = ({item}) => {
       <img src={item.image} alt="" className='work__img'/>
       <h3 className="work__title">{item.title}</h3>
       <a href={item.link} target='blank' className="work__button">
-        demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
+        Explore <i className="bx bx-right-arrow-alt work__button-icon"></i>
       </a>
     </div>
   )
